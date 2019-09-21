dad71ad3e045367d19b011b4de275f9d8057bbcf
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-09-21 18:05:28 +0100

    Create market data microservice

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Signal Generator
 A custom signal generation program.
 # To Run
-export FLASK\_app=market\_data.py
+export FLASK\_ENV=development
+export FLASK\_APP=market\_data.py
+
+flask run
--- src/marketdata/market_data.py
+++ src/marketdata/market_data.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+f = open('IEX_connection_details.txt', 'r')
+API_token = f.read()[:-1]
+f.close()
 
 @app.route('/')
 def index():
-  return 'Server Works!'
+    return 'Server Works!'
   
-@app.route('/greet')
-def say_hello():
-  return 'Hello from Server'
+@app.route('/stock/<ticker>')
+def show_stock_price(ticker: str):
+    query_string = 'https://cloud.iexapis.com/stable/stock/{}/quote?token={}'.format(ticker.lower(), API_token)
+    print(query_string)
+    result = requests.get(url = query_string) 
+    print("Fetching data for {}".format(ticker))
+    return result.json() 
