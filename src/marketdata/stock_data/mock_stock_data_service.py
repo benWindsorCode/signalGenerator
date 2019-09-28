5f2e16933fe2c2b36fc3ab1c5ceea8f4eea0d30f
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-09-28 16:29:48 +0100

    Implement mock market data service

---
+++ src/marketdata/stock_data/mock_stock_data_service.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import requests
+import random
+
+
+app = Flask(__name__)
+high = 300
+close = 500
+latestPrice = 240
+
+def _get_fake_data():
+    data = {
+            'high':high,
+            'close':close,
+            'latestPrice':latestPrice+random.randint(1,10)
+    } 
+    return data
+
+@app.route('/stock/<ticker>')
+def show_stock(ticker: str):
+    print("Fetching data for {}".format(ticker))
+    return _get_fake_data() 
+
+@app.route('/stock/<ticker>/<property>')
+def show_stock_property(ticker: str, property: str):
+    data = _get_fake_data()
+    return str(data[property])
