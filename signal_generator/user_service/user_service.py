cc69a72f3d34d6b4881a23a786084afb1346b026
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-09-29 17:05:13 +0100

    Implement user insertion service

---
+++ signal_generator/user_service/user_service.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import request
+import yaml
+import mysql.connector
+import requests
+
+app = Flask(__name__)
+
+@app.route('/user/add', methods=['POST'])
+def user_add():
+    with open("../config/database_connection_details.yaml", 'r') as yaml_file:
+        cfg = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        
+    mydb = mysql.connector.connect(
+        host=cfg['mysql']['host'],
+        user=cfg['mysql']['user'],
+        passwd=cfg['mysql']['passwd'],
+        database=cfg['mysql']['database']
+    )
+
+    mycursor = mydb.cursor()
+    data = request.get_json()
+
+    sql = "INSERT INTO sig_gen.user (username, sms_number, email) VALUES (%s, %s, %s)"
+    vals = (data['username'], data['sms_number'], data['email'])
+    print(data)
+
+    mycursor.execute(sql, vals)
+    mydb.commit()
+    mydb.disconnect()
+    return 'COMPETE'
