fb4f3a85112a31ee142f9f094af9d03643d97bed
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-09-30 23:29:27 +0100

    Implement get condition in condition_service

--- signal_generator/condition_service/condition_service.py
+++ signal_generator/condition_service/condition_service.py
@@ -7,19 +7,21 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/condition/add', methods=['POST', 'OPTIONS'])
-@cross_origin(origin='localhost')
-def user_add():
+def getDb():
     with open("../config/database_connection_details.yaml", 'r') as yaml_file:
         cfg = yaml.load(yaml_file, Loader=yaml.FullLoader)
         
-    mydb = mysql.connector.connect(
+    return mysql.connector.connect(
         host=cfg['mysql']['host'],
         user=cfg['mysql']['user'],
         passwd=cfg['mysql']['passwd'],
         database=cfg['mysql']['database']
     )
 
+@app.route('/condition/add', methods=['POST', 'OPTIONS'])
+@cross_origin(origin='localhost')
+def condition_add():
+    mydb = getDb()
     mycursor = mydb.cursor()
     data = request.get_json()
 
@@ -31,3 +33,13 @@ def user_add():
     mydb.commit()
     mydb.disconnect()
     return 'COMPETE'
+
+@app.route('/condition/<idcondition>', methods=['GET'])
+def condition_get(idcondition):
+    mydb = getDb()
+    mycursor = mydb.cursor()
+    mycurson.execute('SELECT * FROM sig_gen.condition WHERE idcondition = {}'.format(idcondition))
+    result = mycursor.fetchone()
+    mydb.disconnect()
+    return result
+
