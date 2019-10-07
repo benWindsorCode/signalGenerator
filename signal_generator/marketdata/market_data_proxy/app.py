5297b80f6c5ffe1e9cf945c65a4083d68b2af8b1
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-10-07 08:08:52 +0100

    Fix cloud deployment of market data service

--- signal_generator/marketdata/market_data_proxy/app.py
+++ signal_generator/marketdata/market_data_proxy/app.py
@@ -2,9 +2,10 @@ from flask import Flask
 import requests
 
 app = Flask(__name__)
+hostname ="127.0.0.1"  # use 'host.docker.internal' if testing the containers locally
 
 def _parse_stock(ticker):
-    query_string = 'http://host.docker.internal:5000/stock/{}'.format(ticker) 
+    query_string = 'http://{}:5000/stock/{}'.format(hostname, ticker) 
     result = requests.get(url = query_string) 
     return result.json()
 
@@ -13,11 +14,11 @@ def _parse_currency(symbol):
     base = symbol.split('-')[0]
     secondary = symbol.split('-')[1]
     if base in cryptos:
-        query_string = 'http://host.docker.internal:5001/crypto/{}-{}'.format(base, secondary) 
+        query_string = 'http://{}:5001/crypto/{}-{}'.format(hostname, base, secondary) 
         result = requests.get(url = query_string) 
         return result.json()
     else:
-        query_string = 'http://host.docker.internal:5002/ccy/{}-{}'.format(base, secondary) 
+        query_string = 'http://{}:5002/ccy/{}-{}'.format(hostname, base, secondary) 
         result = requests.get(url = query_string) 
         return result.json()
 
