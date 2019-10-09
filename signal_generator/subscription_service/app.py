17dc89d396b5af2be9bbe9dfe8a7ba236903ad96
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-10-09 08:31:03 +0100

    Contine implementation of subscription service

--- signal_generator/subscription_service/app.py
+++ signal_generator/subscription_service/app.py
@@ -6,12 +6,17 @@ import requests
 app = Flask(__name__)
 sns = boto3.client('sns', region_name='eu-west-1')
 
-@app.route('/subscription/topic/create', methods=['GET', 'POST'])
+@app.route('/subscription/topic/create', methods=['POST'])
 def create_topic():
+    data = request.get_json()
     response = sns.create_topic(
-            Name='test_topic'
+            Name='{}_{}'.format(username, iduser)
     )
+    print(response)
     return "Created"
+
+@app.route('/subscription/add', methods=['POST'])
+def add_subscription():
 
 @app.route('/notify/health')
 def health():
